1aee5bceadb275e7735d28df4664630efbe9746b
Robert Melton <rsmelton@crimson.ua.edu>
2025-10-16 11:18:40 -0500

    Now using semantic html correctly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ import { FaGlobeAmericas } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <div className='flex justify-center items-center gap-2 bg-[#E36560] h-[60px] text-white'>
+    <header className='flex justify-center items-center gap-2 bg-[#E36560] h-[60px] text-white'>
       <FaGlobeAmericas className='text-[30px]' />
       <span className='inter-bold tracking-tighter'>my travel journal.</span>
-    </div>
+    </header>
   )
 };
 
