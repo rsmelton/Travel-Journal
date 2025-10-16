938bbbdf81862be1f499223f2346822fdd40be5a
Robert Melton <rsmelton@crimson.ua.edu>
2025-10-16 12:26:51 -0500

    Fixed header text font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
-import { FaGlobeAmericas } from 'react-icons/fa';
+import { FaGlobeAmericas } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <header className='flex justify-center items-center gap-2 bg-[#F55A5A] h-[60px] text-white'>
-      <FaGlobeAmericas className='text-[30px]' />
-      <span className='inter-bold tracking-tighter'>my travel journal.</span>
+    <header className="flex justify-center items-center gap-2 bg-[#F55A5A] h-[60px] text-white">
+      <FaGlobeAmericas className="text-[30px]" />
+      <span className="inter-regular tracking-tighter">my travel journal.</span>
     </header>
-  )
+  );
 };
 
 export default Header;
