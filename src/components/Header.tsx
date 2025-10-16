1e33580082624b134993a3c6a0b474aec3f0de4e
Robert Melton <rsmelton@crimson.ua.edu>
2025-10-16 11:31:26 -0500

    Changed background color of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { FaGlobeAmericas } from 'react-icons/fa';
 
 const Header = () => {
   return (
-    <header className='flex justify-center items-center gap-2 bg-[#E36560] h-[60px] text-white'>
+    <header className='flex justify-center items-center gap-2 bg-[#F55A5A] h-[60px] text-white'>
       <FaGlobeAmericas className='text-[30px]' />
       <span className='inter-bold tracking-tighter'>my travel journal.</span>
     </header>
