a4a589d31a06f266af6a10af7d5056c6ede2d6cb
Robert Melton <rsmelton@crimson.ua.edu>
2025-10-16 11:13:38 -0500

    Header component complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
+import { FaGlobeAmericas } from 'react-icons/fa';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-center items-center gap-2 bg-[#E36560] h-[60px] text-white'>
+      <FaGlobeAmericas className='text-[30px]' />
+      <span className='inter-bold tracking-tighter'>my travel journal.</span>
+    </div>
   )
 };
 
